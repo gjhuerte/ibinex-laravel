1c7626b53b9ea9a942ac1c11b169f999f002a008
Lester Jan Artienda <lartienda@ad.acquireap>
2018-03-14 17:11:18 +0800

    draft-security

---
+++ resources/views/security/security.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title','Security')
+
+
+@push('stylesheets')
+    <link href="{{ asset("css/security.css") }}" rel="stylesheet" />
+@endpush
+
+@section('header-content')
+    <section id="security-hero-content" class="col-xl-7 col-sm-12">
+        <h1 class="title text-center">Security</h1>
+        <p class="description text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </section>
+@endsection
+
+@section('content')
+@endsection
