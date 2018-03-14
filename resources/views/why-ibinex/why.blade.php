b5891fc4eb242d116dab7d1e7ad2310e122da246
Sean Paolo Flores <sflores@ad.acquireap>
2018-03-14 16:08:35 +0800

    test push

---
+++ resources/views/why-ibinex/why.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title','Why-Ibinex')
+
+
+@push('stylesheets')
+    <link href="{{ asset("css/why.css") }}" rel="stylesheet" />
+@endpush
+
+@section('header-content')
+
+@endsection
+
+@section('content')
+
+@endsection
