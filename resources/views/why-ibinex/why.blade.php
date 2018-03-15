79c1b7707fc62ca4960a2aecc2fd4a756516f728
Sean Paolo Flores <sflores@ad.acquireap>
2018-03-15 11:32:25 +0800

    live demo button update

--- resources/views/why-ibinex/why.blade.php
+++ resources/views/why-ibinex/why.blade.php
@@ -62,7 +62,7 @@
         </div><!--row-->
     </div><!--container-->
     <div class="container-fluid remover">
-        <div class="row position-relative max-width-none">
+        <div  class="row position-relative max-width-none">
             <div class="col-xs-12 blocktext-background shadow"></div>
             <div class="block mx-3 px-3 shadow">
                 <h2 class="blocktext">We fuse innovation with accessibility as blockchain technology evolves.</h2>
