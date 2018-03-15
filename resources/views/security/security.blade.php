5872adef88b1808b650adfb89931449d4a516d6e
lester-ar-ibinex-tech <lartienda@ad.acquireap>
2018-03-15 17:23:27 +0800

    Fixed font sizes and banner spaces @5:34PM

--- resources/views/security/security.blade.php
+++ resources/views/security/security.blade.php
@@ -8,7 +8,7 @@
 @endpush
 
 @section('header-content')
-    <section id="hero-content" class="col-xl-7 col-sm-12" data-aos="zoom-in">
+    <section id="hero-content" class="col-xl-7 col-sm-12">
         <h1 class="title text-center">Security</h1>
         <p class="description text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </section>
