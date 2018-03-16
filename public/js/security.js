9d37cce659103b01c64656215b27fbcb5842d9c4
lester-ar-ibinex-tech <lartienda@ad.acquireap>
2018-03-16 09:56:35 +0800

    Lester - @9:56A

--- public/js/security.js
+++ public/js/security.js
@@ -3,14 +3,14 @@ if (document.documentElement.clientWidth > 1023) {
     window.sr = ScrollReveal({reset: false,viewFactor: 0.3,delay: 1000,useDelay: 'always',});
 
 
-    // title = Security
-    sr.reveal('#security_title', {
-        origin: 'bottom',
-        duration: 2000,
-        distance: '100px',
-        delay: 1200,
-        useDelay: 'always'
-    }).delay(5000);
+    // // title = Security
+    // sr.reveal('#security_title', {
+    //     origin: 'bottom',
+    //     duration: 2000,
+    //     distance: '100px',
+    //     delay: 1200,
+    //     useDelay: 'always'
+    // }).delay(5000);
 
     sr.reveal('#subtitle_1', {
         origin: 'left',
