d89a2c63148050470df57f4628084634c00b2444
Christian Ace Labini <clabini@ad.acquireap>
2018-03-20 10:44:06 +0800

    removed animation

--- public/js/home.js
+++ public/js/home.js
@@ -42,54 +42,54 @@ $(function(){
 
 
 // scrollreveal is disabled on width=1023px below
-    if (document.documentElement.clientWidth > 1023) {
-        window.sr = ScrollReveal({reset: false, delay: 100, duration: 1200,scale: 0.7, distance: '2em', origin: 'bottom'});
-
-        sr.reveal('#second-fold aside img', {
-            origin: 'fadeIn',
-
-        });
-        sr.reveal('#third-fold aside img', {
-            origin: 'left',
-            delay: 300
-        });
-        sr.reveal('#sixth-fold div.brochures', {
-            duration: 1500
-        }, 250);
-        sr.reveal('#fourth-fold aside .embed-image', {
-            duration: 500
-        });
-        sr.reveal('#fourth-fold aside .embed-blue-wrap *', {
-            duration: 700
-        }, 200);
-        sr.reveal('#second-fold > section div.order-xl-1 *', {
-            duration: 300
-        }, 200);
-        sr.reveal('#third-fold > section div.order-xl-12 *', {
-            duration: 300
-        }, 200);
-        sr.reveal('#fourth-fold > section div.order-xl-1 *', {
-            duration: 300
-        }, 200);
-        sr.reveal('#fifth-fold > section div.order-xl-2 *', {
-            duration: 300
-        }, 200);
-        sr.reveal('#fifth-fold aside .embed-image', {
-            duration: 300
-        });
-        sr.reveal('#fifth-fold aside .embed-image .faButton', {
-            duration: 700,
-            afterReveal: function (){
-
-                let playButton = $('.playButton');
-                let playSize =  parseInt(playButton.height()) / 2;
-                if($( document ).width() > 1201)
-                    playButton.css('top', parseInt($('div.embed-image.halfed').height()) / 2 - playSize).css('right', (0 - playSize));
-                else
-                    playButton.css('top', parseInt($('div.embed-image.halfed').height()) / 2 - playSize).css('right', parseInt($('div.embed-image.halfed').width()) / 2 - playSize);
-                playButton.css('opacity', 1)
-            }
-        }, 200);
-    }
+//     if (document.documentElement.clientWidth > 1023) {
+//         window.sr = ScrollReveal({reset: false, delay: 100, duration: 1200,scale: 0.7, distance: '2em', origin: 'bottom'});
+//
+//         sr.reveal('#second-fold aside img', {
+//             origin: 'fadeIn',
+//
+//         });
+//         sr.reveal('#third-fold aside img', {
+//             origin: 'left',
+//             delay: 300
+//         });
+//         sr.reveal('#sixth-fold div.brochures', {
+//             duration: 1500
+//         }, 250);
+//         sr.reveal('#fourth-fold aside .embed-image', {
+//             duration: 500
+//         });
+//         sr.reveal('#fourth-fold aside .embed-blue-wrap *', {
+//             duration: 700
+//         }, 200);
+//         sr.reveal('#second-fold > section div.order-xl-1 *', {
+//             duration: 300
+//         }, 200);
+//         sr.reveal('#third-fold > section div.order-xl-12 *', {
+//             duration: 300
+//         }, 200);
+//         sr.reveal('#fourth-fold > section div.order-xl-1 *', {
+//             duration: 300
+//         }, 200);
+//         sr.reveal('#fifth-fold > section div.order-xl-2 *', {
+//             duration: 300
+//         }, 200);
+//         sr.reveal('#fifth-fold aside .embed-image', {
+//             duration: 300
+//         });
+//         sr.reveal('#fifth-fold aside .embed-image .faButton', {
+//             duration: 700,
+//             afterReveal: function (){
+//
+//                 let playButton = $('.playButton');
+//                 let playSize =  parseInt(playButton.height()) / 2;
+//                 if($( document ).width() > 1201)
+//                     playButton.css('top', parseInt($('div.embed-image.halfed').height()) / 2 - playSize).css('right', (0 - playSize));
+//                 else
+//                     playButton.css('top', parseInt($('div.embed-image.halfed').height()) / 2 - playSize).css('right', parseInt($('div.embed-image.halfed').width()) / 2 - playSize);
+//                 playButton.css('opacity', 1)
+//             }
+//         }, 200);
+//     }
 
 });
