9dfb1e4f419eec4bd469572e8af6c02f9a1390fc
Joel Simpao <jsimpao@ad.acquireap>
2018-03-20 09:40:55 +0800

    Improved alignment and removed pricing animation

--- public/js/pricing.js
+++ public/js/pricing.js
@@ -31,31 +31,4 @@ $(document).ready(function() {
     if ($(window).width() >= 768) {
         alignParagraphs();
     }
-
-    window.sr = ScrollReveal({
-        distance: '0',
-        duration: 800,
-        delay: 200,
-        easing: 'ease-in-out',
-        mobile: false
-    });
-
-    sr.reveal('#pricing-content h2', {
-        scale: 0
-    });
-
-    sr.reveal('#pricing-content article > h5', {
-        origin: 'left',
-        distance: '30px'
-    });
-
-    sr.reveal('#pricing-content article p', {
-        origin: 'bottom',
-        distance: '30px'
-    });
-
-    sr.reveal('#pricing-content > hr', {
-        scale: 0
-    });
-
 });
