233d1b0acfbaf9851887b7a23f1cddbde7afb6c6
Ansell Randell Cruz <arcruz@ad.acquireap>
2018-03-20 17:37:03 +0800

    fixed linkedin button

--- resources/views/about-us/about-us.blade.php
+++ resources/views/about-us/about-us.blade.php
@@ -94,8 +94,8 @@
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiulgod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
     <p class="py-2">
-        <button class="btn blue-ibinex-bg btn-primary round-btn px-4">
-            <i class="fa fa-linkedin-square"></i> Linkedin profile
+        <button class="btn blue-ibinex-bg btn-primary round-btn px-4 py-2 linked-in-btn">
+            <img src="/img/about-us/linked_in.png" /> Linkedin profile
         </button>
     </p>
 </div>
